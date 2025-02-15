5538f47b5b0cbdc5eedd20911b74aa935d550fdb
mariaadyrbekova <mariya.adyrbekova@narxoz.kz>
2025-02-15 12:23:44 +0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Parot parot = new Parot();
+        parot.setName("Kesha");
+        System.out.println(parot.getName());
+    }
+}
