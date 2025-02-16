866a937652f399f50331717a052ac34641229082
mariaadyrbekova <mariya.adyrbekova@narxoz.kz>
2025-02-16 17:35:07 +0500

    My first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+       AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         Person person = context.getBean(Person.class);
         Car car = context.getBean(Car.class);
 
+        person.setName("John");
+        car.setModel("Ford");
         System.out.println(person.getName());
         System.out.println(car.getModel());
     }
