c8ee00a6926e5d3a34b1ce4e182e03e6c3228f4c
mariaadyrbekova <mariya.adyrbekova@narxoz.kz>
2025-02-16 17:11:56 +0500

    Add first bean Person and Car

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
 public class AppConfig {
+    @Bean
+    public Person person() {
+        return new Person();
+    }
 }
